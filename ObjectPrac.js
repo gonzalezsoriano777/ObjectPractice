2c10498b840151e977c84d0917dba1c4350b7fe3
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-08-21 22:53:50 +0000

    Understand it

---
+++ ObjectPrac.js
@@ -0,0 +1,15 @@
+var soccer = {
+    name: "futbol",   
+    color: "blue",
+    height: "small",
+    eyecolor: "black",
+    age: "unknown",
+    sprtname: function() {
+        return this.name + " " + this.color
+    }
+    sportage: function() {
+        return this.age + " " + this.height
+    },
+};
+
+
